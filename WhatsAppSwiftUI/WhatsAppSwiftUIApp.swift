917f20259d3390446920ff80f9346a9b2f12247e
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2024-05-15 13:42:26 +0300

    first commit, project setup and add assets

---
+++ WhatsAppSwiftUI/WhatsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppSwiftUIApp.swift
+//  WhatsAppSwiftUI
+//
+//  Created by HardiB.Salih on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
